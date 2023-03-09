7e54d0a1b7c2eb7e7593785c333ce590a65e377a
HMBond <hubertusmariabond@gmail.com>
2023-03-09 23:12:16 +0100

    No clutter in output log on production

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { randomUUID } from 'crypto';
 import * as dotenv from 'dotenv';
-import { WebSocket, WebSocketServer } from 'ws';
+import { RawData, WebSocket, WebSocketServer } from 'ws';
 import { connectMidi, note, pitchBend } from './midi';
 import { ErrorResponse, Message } from './types';
 
@@ -13,41 +13,50 @@ const wss = new WebSocketServer({
 const midiOut = connectMidi();
 const room: Map<string, WebSocket> = new Map();
 
-wss.on('connection', (ws) => {
+wss.on('connection', (ws: WebSocket) => {
   const id = randomUUID();
   room.set(id, ws);
 
-  ws.on('message', (data) => {
-    try {
-      const message: Message = JSON.parse(data.toString());
-      if (message.type === 'note') {
-        note(midiOut, message);
-      }
-      if (message.type === 'pitchbend') {
-        pitchBend(midiOut, message);
-      }
-      if (message.type === 'share') {
-        for (const [key, socket] of room) {
-          if (key === id) continue;
-          socket.send(JSON.stringify(message));
-        }
-      }
-    } catch (error) {
-      const message = '[MMC-server] could not parse message: ' + data.toString();
-      console.log(message, error.stack);
-      sendError(ws, message);
-      return;
-    }
-    console.log(`[MMC-server] received from ${id.slice(0, 5)}...: %s`, data);
-  });
+  ws.on('message', (data) => handleMessage(data, ws, id));
 
-  console.log(`[MMC-server] New Connection, total: ${room.size}, id: ${id.slice(0, 5)}...`);
+  if (process.env.ENVIRONMENT !== 'production') {
+    console.log(`[MMC-server] New Connection, total: ${room.size}, id: ${id.slice(0, 5)}...`);
+  }
+
   ws.send('Connected');
 
   ws.on('close', () => {
     room.delete(id);
   });
 });
+
+function handleMessage(data: RawData, ws: WebSocket, id: string) {
+  try {
+    const message: Message = JSON.parse(data.toString());
+    if (message.type === 'note') {
+      note(midiOut, message);
+    }
+    if (message.type === 'pitchbend') {
+      pitchBend(midiOut, message);
+    }
+    if (message.type === 'share') {
+      for (const [key, socket] of room) {
+        if (key === id) continue;
+        socket.send(JSON.stringify(message));
+      }
+    }
+  } catch (error) {
+    const message = '[MMC-server] could not parse message: ' + data.toString();
+    if (process.env.ENVIRONMENT !== 'production') {
+      console.log(message, error.stack);
+    }
+    sendError(ws, message);
+    return;
+  }
+  if (process.env.ENVIRONMENT !== 'production') {
+    console.log(`[MMC-server] received from ${id.slice(0, 5)}...: %s`, data);
+  }
+}
 
 function sendError(ws: WebSocket, message: string, status = 500) {
   const error: ErrorResponse = {
